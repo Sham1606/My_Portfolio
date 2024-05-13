a9a829ea20e6a4ef729a85fa4a468779078af12a
Sham1606 <shamganesh806@gmail.com>
2024-05-13 23:40:41 +0530

    commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,4 @@
 import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
 import traffic from "../assets/projects/traffic.jpeg";
 import auto from "../assets/projects/auto.jpeg";
 import chirping_hub from "../assets/projects/chirping_hub.png"
